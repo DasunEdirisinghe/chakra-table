b3f7567c0f8377f88cd68829d427a7bcf11f67ef
DasunEdirisinghe <edirisinghe954@gmail.com>
2022-02-20 10:26:16 +0530

    fix: build error

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import * as serviceWorker from "./serviceWorker"
 
 ReactDOM.render(
   <React.StrictMode>
+    <ColorModeScript />
     <App />
   </React.StrictMode>,
   document.getElementById("root"),
